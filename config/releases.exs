61b8b62e7ea9a4876e44020cbdf8d3a61495e7f7
Icaro Pereira <icarooliv2@gmail.com>
2021-06-01 21:36:02 -0300

    fix releases file

--- config/releases.exs
+++ config/releases.exs
@@ -1,8 +1,4 @@
-# In this file, we load production configuration and secrets
-# from environment variables. You can also hardcode secrets,
-# although such is generally not recommended and you have to
-# remember to add this file to your .gitignore.
-use Mix.Config
+import Config
 
 database_url =
   System.get_env("DATABASE_URL") ||
@@ -36,7 +32,7 @@ config :team_budget, TeamBudgetWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :team_budget, TeamBudgetWeb.Endpoint, server: true
+  config :team_budget, TeamBudgetWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
